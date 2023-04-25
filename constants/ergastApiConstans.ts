9b8f9ed36c222e9aa61da554df15a45e36a1ac94
Serhii <avatarcapa@gmail.com>
2023-04-25 13:24:21 +0200

    Introduced ergast API endpoints: ALL_SEASON_WINNERS_PATH and SEASON_WINNERS_PATH

---
+++ constants/ergastApiConstans.ts
@@ -0,0 +1,5 @@
+export const ROOT_ERGAST_API_PATH = 'https://ergast.com/api/f1';
+
+export const ALL_SEASON_WINNERS_PATH = '/driverStandings/1.json';
+
+export const SEASON_WINNERS_PATH = '/{season}/results/1.json';
