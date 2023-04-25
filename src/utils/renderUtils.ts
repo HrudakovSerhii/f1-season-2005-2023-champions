b9c5d188196f8dcd11cbb0f9dd6c6351c0c0b6ea
Serhii <avatarcapa@gmail.com>
2023-04-25 17:43:32 +0200

    renderUtils.ts getSeasonsWinnersGroupedData to group SeasonsWinners cards by provided group size (experimental feature)

---
+++ src/utils/renderUtils.ts
@@ -0,0 +1,17 @@
+import { StandingItem } from '../types'
+
+export const getSeasonsWinnersGroupedData = (data: StandingItem[], groupSize: number = 5) =>
+    data?.reduce((groupedItems, standingItem, index) => {
+        const groupIndex = Math.floor(index / groupSize)
+
+        if (groupedItems[groupIndex]) {
+            groupedItems[groupIndex] = [
+                ...groupedItems[groupIndex],
+                data[index],
+            ]
+        } else {
+            groupedItems.push([data[index]])
+        }
+
+        return groupedItems
+    }, [] as StandingItem[][])
