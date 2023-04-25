ea054579e454fa7e418b0bb1a978de53169847e2
Serhii <avatarcapa@gmail.com>
2023-04-25 21:00:42 +0200

    Updated ScreenTitle position for wide screens

--- src/components/ScreenTitle.tsx
+++ src/components/ScreenTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ScreenTitle: React.FC<{ title: string }> = ({ title }) => {
     return (
-        <div role="textbox" className="w-[100%] p-4">
+        <div role="textbox" className="w-full max-w-screen-xl p-4">
             <h2 className="text-lg text-gray-600 font-medium">{title}</h2>
         </div>
     )
