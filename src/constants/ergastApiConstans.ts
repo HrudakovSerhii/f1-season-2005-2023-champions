5de1cc41dc7c5d7766dbe34340faf5d6023dd272
Serhii <avatarcapa@gmail.com>
2023-04-25 18:11:42 +0200

    Fixed SEASON_WINNERS_PATH replace pattern

--- src/constants/ergastApiConstans.ts
+++ src/constants/ergastApiConstans.ts
@@ -1,5 +1,5 @@
-export const ROOT_ERGAST_API_PATH = 'https://ergast.com/api/f1';
+export const ROOT_ERGAST_API_PATH = 'https://ergast.com/api/f1'
 
-export const ALL_SEASON_WINNERS_PATH = '/driverStandings/1.json';
+export const ALL_SEASON_WINNERS_PATH = '/driverStandings/1.json'
 
-export const SEASON_WINNERS_PATH = '/{season}/results/1.json';
+export const SEASON_WINNERS_PATH = '/{seasonId}/results/1.json'
