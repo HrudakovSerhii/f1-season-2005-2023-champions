53b9042b835ed0063239c4fd49fe60e3885bb3a2
Serhii <avatarcapa@gmail.com>
2023-04-25 14:02:29 +0200

    Constructed Ergast API types using external tools

---
+++ src/types/index.ts
@@ -0,0 +1,76 @@
+export interface MRData {
+    limit: string
+    offset: string
+    series: string
+    total: string
+    url: string
+    xmlns: string
+}
+
+export interface Driver {
+    url: string
+    driverId: string
+    givenName: string
+    familyName: string
+    dateOfBirth: string
+    nationality: string
+}
+
+export interface Constructor {
+    constructorId: string
+    url: string
+    name: string
+    nationality: string
+}
+
+export interface DriverStanding {
+    position: string
+    positionText: string
+    points: string
+    wins: string
+    Driver: Driver
+    Constructors: Constructor[]
+}
+
+export interface RaceResult {
+    number: string
+    position: string
+    points: string
+    Driver: Driver
+    Constructor: Constructor
+    laps: string
+    Time: {
+        millis: string
+        time: string
+    }
+}
+
+export interface Race {
+    date: string
+    raceName: string
+    round: string
+    season: string
+    time: string
+    url: string
+    Results: RaceResult[]
+}
+
+export interface StandingItem {
+    round: string
+    season: string
+    DriverStandings: DriverStanding[]
+}
+
+export interface SeasonMRData extends MRData {
+    RaceTable: {
+        position: string
+        season: string
+        Races: Race[]
+    }
+}
+
+export interface AllSeasonsMRData extends MRData {
+    StandingsTable: {
+        StandingsLists: StandingItem[]
+    }
+}
